4ebfbe0040e9d107060ca83f7170388ab1b57652
Michalec2 <m.wechmann@interia.pl>
2024-10-02 18:12:13 +0200

    Projekt

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace players_catalog.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
